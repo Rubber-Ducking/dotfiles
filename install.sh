d780fe1ee12ecbcd90a7ab682667f7956fe178fc
Niklas Häusele <niklas.haeusele@gmail.com>
2014-02-21 00:38:08 +0100

    added install.sh added the bundle to backup

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#directory
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="vimrc vim zshrc" #list of files I want to backup
+
+echo "create $olddir"
+mkdir -p $olddir
+echo "done"
+
+echo "Cd to $dir directory"
+cd $dir
+echo "done"
+
+for file in $files; do
+    echo "moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old
+    echo "Creating the symlinks in home"
+    ln -s $dir/$file ~/.$file
+done
